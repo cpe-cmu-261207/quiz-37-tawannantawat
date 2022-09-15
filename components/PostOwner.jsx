374a1ac42af543d649ce0716f17ed1a8430e3795
tawannantawat <tawanswc@gmail.com>
2022-09-15 14:54:44 +0700

    update

--- components/PostOwner.jsx
+++ components/PostOwner.jsx
@@ -1,5 +1,22 @@
 import React from "react";
 
 export default function PostOwner(props) {
-  return <div></div>;
+  return <div className="vstack gap-3">
+  <div className="d-flex align-items-center gap-3">
+    <img
+      src="/profileImages/handsome.jpg"
+      width="48"
+      height="48"
+      className="rounded-circle"
+      style={{ objectFit: "cover" }}
+    />
+    <span className="fw-semibold fs-5">{props.name}</span>
+  </div>
+  <span>{props.massage}</span>
+  <div className="d-flex align-items-center gap-1">
+    <img src="/like.svg" width={20}></img>
+    <span className="text-muted">{props.likes}</span>
+  </div>
+  <hr className="m-0" />
+</div>;
 }
